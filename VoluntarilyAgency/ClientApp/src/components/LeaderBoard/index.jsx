0a21684e495d2c5d5cf8d3f38c87ed75a381d55a
Eddie Chiang <eddiecjc@gmail.com>
2018-12-09 12:04:58 +1300

    Added drop down for leaderboard period selection.

--- VoluntarilyAgency/ClientApp/src/components/LeaderBoard/index.jsx
+++ VoluntarilyAgency/ClientApp/src/components/LeaderBoard/index.jsx
@@ -4,7 +4,15 @@ export default class LeaderBoard extends Component {
     render() {
         return (
             <div>
-                <h1>{"Leader Board"}</h1>
+                <h1>{"Leaderboard"}</h1>
+                <div>
+                    <p>Period</p>
+                    <select>
+                        <option value="1-month">1 month</option>
+                        <option value="1-year">1 year</option>
+                        <option value="all-time">All Time</option>
+                    </select>
+                </div>
             </div>
         );
     }
